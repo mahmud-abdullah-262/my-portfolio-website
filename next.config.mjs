085d7f0275c38e344b5523b60e5215993303b608
mahmud-abdullah-262 <abdullah624683@gmail.com>
2026-05-12 12:40:34 +0600

    next js native routing

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+   output: 'standalone',
+  redirects: async () => {
+    return [
+      {
+        source: '/old-page',
+        destination: '/new-page',
+        permanent: true,
+      },
+    ]
+  },
+
   images: {
     remotePatterns: [
       {
